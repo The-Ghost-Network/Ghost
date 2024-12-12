fc7db3553383dc33c598b27197a7eaf0d3cbfcf2
Nobodycares-lo <admin@nobodycareslo.me>
2024-12-12 16:05:35 -0700

    Fix navbar on cloudflare pages

--- assets/js/theory.js
+++ assets/js/theory.js
@@ -5,13 +5,13 @@ function createnav() {
   <div class="container">
         <img onclick="window.location.href='/'" style="width:50px;" src="/assets/img/ghost.png" class="mainIMG"></img>
         <ul class="nav-links">
-          <li><a href="/c.html/" id="school"><i class="fa-solid fa-gamepad active"></i> Games</a></li>
+          <li><a href="/c/" id="school"><i class="fa-solid fa-gamepad active"></i> Games</a></li>
           <span style="color: var(--Background-Color); opacity: 75%;">|</span>
-          <li><a href="/a.html/" id="algebra"><i class="fas fa-shapes active"></i> Apps</a></li>
+          <li><a href="/a/" id="algebra"><i class="fas fa-shapes active"></i> Apps</a></li>
           <span style="color: var(--Background-Color); opacity: 75%;">|</span>
-         <li><a href="/s.html/" id="sett"><i class="fa-solid fa-gear active"></i> Settings</a></li>
+         <li><a href="/s/" id="sett"><i class="fa-solid fa-gear active"></i> Settings</a></li>
           <span style="color: var(--Background-Color); opacity: 75%;">|</span>
-         <li><a href="/w.html/" id="brows"><i class="fa-solid fa-search active"></i>Sea<x>rch</a></li>
+         <li><a href="/w/" id="brows"><i class="fa-solid fa-search active"></i>Sea<x>rch</a></li>
           <span style="color: var(--Background-Color); opacity: 75%;">|</span>
          <li><a href="https://discord.gg/dbyDXfs5dN" id="brows"><i class="fa-brands fa-discord active" style="margin-right: 5px;"></i></a></li>
          <li><a href="https://github.com/The-Ghost-Network/Ghost-Node" id="brows"><i class="fa-brands fa-github active"></i></x></a></li>
